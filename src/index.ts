6096c2b3ecc6565af1ac89c3a4d56d7f257f52bc
M. Irfan @mirfan.exe <dev.mirfan@gmail.com>
2024-03-05 11:51:22 +0700

    Init - create mqtt-broker support websocket

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import Aedes from "aedes";
+import { createServer } from "aedes-server-factory";
+
+const portMQTT: number = 1883;
+const portWS: number = 8083;
+
+const aedes = new Aedes();
+
+aedes.on("clientError", (client, error) => {
+  console.error("TERJADI ERROR CLIENT", error);
+});
+aedes.on("client", (client) => {
+  console.log("Client Connect", client.id);
+});
+aedes.on("clientDisconnect", (client) => {
+  console.log("Client Disconnect", client.id);
+});
+
+const httpServer = createServer(aedes, { ws: true });
+const broker = createServer(aedes);
+
+broker.listen(portMQTT, () => {
+  console.log("MQTT-Broker is ready at", portMQTT);
+});
+
+httpServer.listen(portWS, () => {
+  console.log("Websocket is ready at", portWS);
+});
